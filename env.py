966f020e553fd4b6ba6212ed1be54404b47bcc6f
Kes Cardoso <kesia.development@gmail.com>
2020-05-01 21:04:39 +0000

    Git ignore file

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://ngnezzuepgeyah:e5a4a26195d1f0595555ed71085bc3081d4391cb707fdc674c85beda284202a0@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/dfsbb0nokji39v'
