2c4fd513e3c31ed3126352c5c0b84ec1cc71890a
Kes Cardoso <kesia.development@gmail.com>
2020-05-02 10:03:17 +0000

    Untrack file

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ["HOSTNAME"] = '0.0.0.0'
-os.environ["PORT"] = '5000'
-os.environ["SECRET_KEY"] = 'b(4jhvu3jq*$bia&7r%hu&=po60ve(--z22^!zjwd0_aly&f$l'
-os.environ["DEVELOPMENT"] = '1'
-os.environ["DATABASE_URL"] = 'postgres://ngnezzuepgeyah:e5a4a26195d1f0595555ed71085bc3081d4391cb707fdc674c85beda284202a0@ec2-46-137-84-140.eu-west-1.compute.amazonaws.com:5432/dfsbb0nokji39v'
